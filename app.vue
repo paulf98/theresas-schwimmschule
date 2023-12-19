01daebfea2b47a64328980311f2e33190e331360
Paul Faass <paulfaass@gmail.com>
2023-12-19 14:22:00 +0100

    feat: create components dir with navbar placeholder

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div class="w-full h-20 bg-red-400">
-
-  </div>
+  <Navbar />
 </template>
---
+++ components/Navbar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-full h-20 bg-green-200 flex justify-center items-center">
+        NAVBAR
+    </div>
+</template>
