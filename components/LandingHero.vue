f8776e404ad4766dd29c8d16fba52eed570ea61e
Paul Faass <paulfaass@gmail.com>
2023-12-20 20:32:43 +0100

    fix: improve the navbars responsiveness

--- components/LandingHero.vue
+++ components/LandingHero.vue
@@ -1,26 +1,27 @@
 <template>
-    <section>
-        <div class="flex flex-col md:flex-row bg-cyan-50 items-center justify-around py-8 px-4 md:px-8 gap-8">
-            <div class="flex flex-col flex-1 justify-center max-w-sm">
-                <Heading>Theresa's Schwimmschule</Heading>
-                <Subheading>
-                    Wo Kinder zu Schwimmern werden
-                </Subheading>
-                <Button class="mt-6">
-                    Jetzt anmelden
-                </Button>
+    <div>
+        <section class="bg-cyan-50">
+            <div class="flex flex-col md:flex-row items-center justify-around py-8 px-4 md:px-8 gap-8 max-w-5xl mx-auto">
+                <div class="flex flex-col flex-1 justify-center max-w-sm">
+                    <Heading>Theresa's Schwimmschule</Heading>
+                    <Subheading>
+                        Wo Kinder zu Schwimmern werden
+                    </Subheading>
+                    <Button class="mt-6">
+                        Jetzt anmelden
+                    </Button>
+                </div>
+
+                <NuxtImg class="rounded-bl-full rounded-tr-full rounded-tl-full max-w-sm" src="/hero-image.jpg" />
+
             </div>
-
-            <NuxtImg class="rounded-bl-full rounded-tr-full rounded-tl-full max-w-sm" src="/hero-image.jpg" />
-
-        </div>
-
+        </section>
         <svg xmlns="http://www.w3.org/2000/svg" class="fill-current text-cyan-50" viewBox="0 0 1000 99"
             preserveAspectRatio="none">
             <path
                 d="M526.35,17.11C607.41,28.38,687,48.17,768.06,59.5A1149.19,1149.19,0,0,0,1000,68.07V0H0V99C155.18,13.84,347.42-7.77,526.35,17.11Z"
                 transform="translate(0 0.04)"></path>
         </svg>
-    </section>
+    </div>
 </template>
 
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,17 +1,83 @@
 <template>
-    <div class="bg-cyan-50 py-2">
-        <nav class="w-[80%] mx-auto h-16 bg-white flex justify-between items-center shadow-md rounded-full z-10 px-4">
-            <NuxtLink to="/">
-                <div class="bg-sky-700 h-14 w-14 rounded-full" />
-            </NuxtLink>
-            <div class="flex gap-8">
-                <NavLink to="/">Über mich</NavLink>
-                <NavLink to="/about">Leistungen</NavLink>
-                <NavLink to="/contact">Kurse</NavLink>
+    <div class="h-20 bg-cyan-50">
+        <div class="fixed w-full top-0 py-2">
+            <nav
+                class="w-[96%] md:w-[80%] relative max-w-5xl mx-auto h-16 bg-white flex justify-between items-center shadow-md rounded-full z-50 px-2 md:px-4 gap-4">
+                <div class="flex flex-shrink gap-2">
+                    <NuxtLink to="/">
+                        <div class="bg-sky-700 h-14 w-14 rounded-full" />
+                    </NuxtLink>
+                    <UButton variant="ghost" square class="flex md:hidden rounded-full w-14 h-14 justify-center" size="xl"
+                        icon="i-heroicons-bars-3" @click="isMenuOpen = true" />
+                </div>
+                <div class="hidden md:flex gap-8">
+                    <NavLink v-for="link in links" :to="link.to">{{ link.label }}</NavLink>
+                </div>
+
+                <Button>
+                    Zur Anmeldung
+                </Button>
+            </nav>
+        </div>
+
+        <USlideover v-model="isMenuOpen">
+            <div class="p-4 flex-1">
+                <UButton variant="ghost" square class="rounded-full w-14 h-14 flex justify-center mb-4" size="xl"
+                    icon="i-heroicons-x-mark" @click="isMenuOpen = false" />
+                <UVerticalNavigation :links="links" />
             </div>
-            <Button>
-                Zur Anmeldung
-            </Button>
-        </nav>
+        </USlideover>
     </div>
 </template>
+
+<script setup lang="ts">
+const isMenuOpen = ref(false)
+
+const links = [
+    {
+        label: 'Über mich',
+        to: '/'
+    },
+    {
+        label: 'Leistungen',
+        to: '/about'
+    },
+    {
+        label: 'Kurse',
+        to: '/contact'
+    }
+]
+
+
+function hideAndShowNavOnScroll() {
+    let lastScrollTop = 0
+    const navbar = document.querySelector('nav')
+    if (!navbar) return
+
+    window.addEventListener('scroll', () => {
+        const scrollTop = document.documentElement.scrollTop
+
+        if (scrollTop > lastScrollTop) {
+            navbar.classList.add('opacity-0')
+        } else {
+            navbar.classList.remove('opacity-0')
+        }
+
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
+    })
+
+
+
+}
+
+onMounted(() => {
+    hideAndShowNavOnScroll()
+})
+
+</script>
+
+<style scoped lang="scss">
+nav {
+    transition: all .25s ease-in-out;
+}
+</style>
