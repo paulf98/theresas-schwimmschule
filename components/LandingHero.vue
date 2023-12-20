d93a4e5bdec885304bd2cd090d9f10bd56681288
Paul Faass <paulfaass@gmail.com>
2023-12-20 13:47:55 +0100

    feat(hero): start implementing a basic hero image

---
+++ components/LandingHero.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <div class="flex bg-cyan-50">
+            <div class="flex flex-col justify-center px-8">
+                <h1>Hero</h1>
+                <h5>subheading</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione sapiente rem officia facere laudantium
+                    molestias aperiam!</p>
+            </div>
+
+            <NuxtImg class="rounded-bl-full max-w-sm" src="/hero-image.jpg" />
+
+        </div>
+
+        <svg xmlns="http://www.w3.org/2000/svg" class="fill-current text-cyan-50" viewBox="0 0 1000 99"
+            preserveAspectRatio="none">
+            <path
+                d="M526.35,17.11C607.41,28.38,687,48.17,768.06,59.5A1149.19,1149.19,0,0,0,1000,68.07V0H0V99C155.18,13.84,347.42-7.77,526.35,17.11Z"
+                transform="translate(0 0.04)"></path>
+        </svg>
+    </section>
+</template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <main>
+        <LandingHero />
         <section>
             <RegistrationForm />
         </section>
