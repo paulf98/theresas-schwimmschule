12e8047a8dd3b19f9d086598348d80bd34106988
Paul Faass <paulfaass@gmail.com>
2023-12-23 09:56:11 +0100

    refactor: adjust the registration form

--- components/RegistrationForm.vue
+++ components/RegistrationForm.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="container mx-auto p-8 max-w-md border rounded-lg shadow-md">
+        <h3 class="mb-4 text-2xl">Anmeldeformular</h3>
         <UForm :schema="schema" :state="state" class="space-y-4" @submit.prevent="submit">
-            <div class="flex gap-8 justify-between">
-                <UFormGroup label="Erziehungsberechtigte/r" name="parentName" eager-validation>
-                    <UInput v-model="state.parentName" placeholder="Name Erziehungsberechtigte/r" />
+
+            <h4 class="mb-4 !mt-8 text-xl">Kursteilnehmer/in</h4>
+            <UFormGroup label="Name Kursteilnehmer/in" name="childName" eager-validation>
+                <UInput v-model="state.childName" placeholder="Name Kursteilnehmer/in" />
+            </UFormGroup>
+            <div class="flex justify-between gap-4">
+                <UFormGroup label="Vorhandene Schwimmabzeichen" name="childExperience" eager-validation class="flex-1">
+                    <UInput v-model="state.childExperience" placeholder="Seepferdchen" />
                 </UFormGroup>
-                <UFormGroup label="Kind" name="childName" eager-validation>
-                    <UInput v-model="state.childName" placeholder="Name Kind" />
-                </UFormGroup>
-            </div>
-            <div class="flex justify-between gap-8">
-                <UFormGroup label="Erfahrung Kind" name="childExperience" eager-validation class="flex-1">
-                    <UInput v-model="state.childExperience" placeholder="Erfahrung Kind" />
-                </UFormGroup>
-                <UFormGroup label="Alter Kind" name="childAge" eager-validation class="flex-shrink w-20">
-                    <UInput type="number" v-model="state.childAge" placeholder="Alter Kind" />
+                <UFormGroup label="Geburtsdatum" name="childBirthday" eager-validation>
+                    <UInput type="date" v-model="state.childBirthday" placeholder="Geburtsdatum" />
                 </UFormGroup>
             </div>
-            <UFormGroup label="Ziel des Kurses" name="courseGoal" eager-validation>
-                <USelect placeholder="Ziel des Kurses" v-model="state.courseGoal" :options="courseGoalOptions" />
+
+            <h4 class="mb-4 !mt-8 text-xl">Erziehungsberechtigte/r</h4>
+            <UFormGroup label="Name Erziehungsberechtigte/r" name="parentName" eager-validation>
+                <UInput v-model="state.parentName" placeholder="Name Erziehungsberechtigte/r" />
             </UFormGroup>
             <UFormGroup label="Telefonnummer" name="phone" eager-validation>
                 <UInput v-model="state.phone" placeholder="Telefonnummer" />
@@ -26,11 +26,14 @@
             <UFormGroup label="E-Mail-Adresse" name="email" eager-validation>
                 <UInput v-model="state.email" placeholder="E-Mail-Adresse" />
             </UFormGroup>
+            <UFormGroup label="Ziel des Kurses" name="courseGoal" eager-validation>
+                <USelect placeholder="Ziel des Kurses" v-model="state.courseGoal" :options="courseGoalOptions" />
+            </UFormGroup>
             <div class="flex flex-col gap-4 items-start">
                 <UButton type="submit" :disabled="!token"
-                    class="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-full">Senden
+                    class="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-full">Anmeldung senden
                 </UButton>
-                <NuxtTurnstile v-model="token" theme="light" />
+                <NuxtTurnstile v-model="token" :options="{ theme: 'light' }" class="self-center" />
             </div>
         </UForm>
     </div>
@@ -57,7 +60,7 @@ function submit() {
 
         Name Erziehungsberechtigte/r: ${state.parentName}
         Name Kind: ${state.childName}
-        Alter Kind: ${state.childAge}
+        Alter Kind: ${state.childBirthday}
         Erfahrung Kind: ${state.childExperience}
         Ziel des Kurses: ${state.courseGoal}
         Telefonnummer: ${state.phone}
@@ -93,7 +96,7 @@ const courseGoalOptions = [
 const schema = z.object({
     parentName: z.string().min(1, { message: 'Bitte geben Sie den Namen des Erziehungsberechtigten an.' }),
     childName: z.string().min(1, { message: 'Bitte geben Sie den Namen des Kindes an.' }),
-    childAge: z.number().min(1, { message: 'Bitte geben Sie das Alter des Kindes an.' }),
+    childBirthday: z.string().min(1, { message: 'Bitte geben Sie das Geburtsdatum des Kindes an.' }),
     childExperience: z.string().min(1, { message: 'Bitte geben Sie die Erfahrung des Kindes an.' }),
     courseGoal: z.nativeEnum(CourseGoal.enum),
     phone: z.string().min(1, { message: 'Bitte geben Sie Ihre Telefonnummer an.' }),
@@ -113,8 +116,8 @@ const defaultState = reactive({
 const state = reactive({
     parentName: "John Doe",
     childName: "Harry Potter",
-    childAge: 4,
-    childExperience: "Okay",
+    childBirthday: 4,
+    childExperience: "Seepferdchen",
     courseGoal: courseGoalOptions[0].value,
     phone: "123456",
     email: "test@example.com",
